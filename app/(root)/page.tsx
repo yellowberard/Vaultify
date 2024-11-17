fc578f2a4b0ec605771b65ee099fead0a2753599
yellowberard <goelmanu002@gmail.com>
2024-11-17 14:12:48 +0530

    Added sign UP

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <div>
         <h1 className={"text-3xl text-brand-100"}>
-          Vaultify - The storage solutions
+          Vaultify - The storage solutions hello
         </h1>
       </div>
     </>
