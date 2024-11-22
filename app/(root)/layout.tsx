805c97c50996e01dceb70643207d1f03db6c0e4d
yellowberard <goelmanu002@gmail.com>
2024-11-23 00:44:58 +0530

    final commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // because next js thnks its static and we cant use cookies so then we will use dynamic and render on server
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
