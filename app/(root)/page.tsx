6398e659e6276122aaff66d4fdd78c79ffbad813
yellowberard <goelmanu002@gmail.com>
2024-11-22 17:05:55 +0530

    file uploader

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Header from "@/components/Header";
-
 export default function Home() {
   return (
     <>
---
+++ components/Sort.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sort = () => {
+  return <div>Sorted</div>;
+};
+
+export default Sort;
